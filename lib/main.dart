f0716f85b8679c3d7e05120ed1443ca5fd7fe3a4
Lyka0000 <lykamaemanguilimotan631@gmail.com>
2025-09-19 10:38:41 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
             title: Text('Lykamae A. Manguilimotan'),
-            backgroundColor: Color.fromARGB(0, 84, 238, 238),
+            backgroundColor: Color.fromARGB(255, 84, 238, 238),
             actions: [
                 IconButton(
                     icon: Icon(Icons.search),
@@ -33,54 +33,73 @@ class MyApp extends StatelessWidget {
             child: Column(mainAxisAlignment: MainAxisAlignment.center,
             children: [
                 Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
                     Icon(Icons.home, size:40, color: Colors.blue),
                     Icon(Icons.favorite, size:40, color: Colors.red),
                     Icon(Icons.music_note_rounded, size:40, color: Colors.green),
                   ],
                   ),
-                  Column(
+              SizedBox(height: 20),
+                  Row(
                     children: [
                       Expanded(
                         child: Container(
                           height: 100,
                           color: Colors.blue,
                           child: Center(
-                            child: Text("Expanded",style: TextStyle(color: Colors.orange),)
-                          ,)
-                        ),)
+                            child: Text("Expanded", style: TextStyle(color: Colors.orange),
+                            ),
+                          ),
+                        ),
+                      ),
                     ],
                   ),    
+              SizedBox(height: 20),
                   Container(
                     height: 100,
-                    width: double.infinity,
+                    width: 150,
                     color: Colors.purple,
                     margin: EdgeInsets.all(16),
                     padding: EdgeInsets.all(16),
                   ),
                   Container(
                     height: 100,
-                    width: double.infinity,
+                    width: 150,
                     color: Colors.amber,
                     margin: EdgeInsets.all(16),
                     padding: EdgeInsets.all(16),
                   ),
-                  //Text
+                  Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Text(
+                      "Styled Text",
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                ),
+              
+              SizedBox(height: 20),
+              Image.network(
+                "https://images.pexels.com/photos/733416/pexels-photo-733416.jpeg",
+                height: 150,
+              ),
             ],
-
+          ),
+        ),
+        
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: "Home",
             ),
+             BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: "Profile",
             ),
-        bottomNavigationBar: BottomAppBar(
-        color: Colors.blue,
-        child: Padding(
-          padding: EdgeInsets.all(16.0),
-         SizedBox(height: 10),
-          Image.network(
-            "https://images.pexels.com/photos/733416/pexels-photo-733416.jpeg",
-            height: 150,
+          ],
         ),
       ),
-        ),
     );
   }
 }
